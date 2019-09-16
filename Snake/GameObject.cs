96e5025d7f15526905ffe4aee5eae2daf840a5f5
Overlord9001 <45371858+Overlord9001@users.noreply.github.com>
2019-09-16 12:57:51 +0200

    fjernede dobbelt using

--- Snake/GameObject.cs
+++ Snake/GameObject.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 
