9cde97e61150fc65da90ef524afb60f6b31e173a
Overlord9001 <45371858+Overlord9001@users.noreply.github.com>
2019-09-16 15:16:00 +0200

    no crash please

--- Snake/Snakehead.cs
+++ Snake/Snakehead.cs
@@ -13,9 +13,7 @@ namespace Snake
     class Snakehead : Snake
     {
         Vector2 savedDirection;
-        GameObject newTile = GameWorld.TileSet[4, 3];
-        Vector2 newTileCoords = new Vector2(4, 3);
-        GameObject oldTile = GameWorld.TileSet[3, 3];
+        
 
         public Snakehead(Vector2 position, string spriteName, ContentManager content) : base(position, spriteName, content)
         {
@@ -35,35 +33,27 @@ namespace Snake
                     direction = savedDirection;
                     savedDirection = Vector2.Zero;
                 }
-
                 oldTile = newTile;
+                newTile.position += direction * 30;
             }
 
             position += direction;
 
             if (Keyboard.GetState().IsKeyDown(Keys.W))
             {
-                newTileCoords.Y -= 1;
                 savedDirection = new Vector2(0, -1);
-                newTile = GameWorld.TileSet[(int)newTileCoords.X, (int)newTileCoords.Y];
             }
             if (Keyboard.GetState().IsKeyDown(Keys.S))
             {
-                newTileCoords.Y += 1;
                 savedDirection = new Vector2(0, 1);
-                newTile = GameWorld.TileSet[(int)newTileCoords.X, (int)newTileCoords.Y];
             }
             if (Keyboard.GetState().IsKeyDown(Keys.A))
             {
-                newTileCoords.X -= 1;
                 savedDirection = new Vector2(-1, 0);
-                newTile = GameWorld.TileSet[(int)newTileCoords.X, (int)newTileCoords.Y];
             }
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
-                newTileCoords.Y += 1;
                 savedDirection = new Vector2(1, 0);
-                newTile = GameWorld.TileSet[(int)newTileCoords.X, (int)newTileCoords.Y];
             }
             base.Update(gameTime);
         }
