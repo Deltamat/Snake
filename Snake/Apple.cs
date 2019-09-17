337d907495c1bcdf6951384cab0b604d19f64168
Mikkel Laurits Jakobsen <mikk514k@student.eadania.dk>
2019-09-17 14:32:15 +0200

    Apples!

--- Snake/Apple.cs
+++ Snake/Apple.cs
@@ -11,6 +11,11 @@ namespace Snake
 {
     class Apple : GameObject
     {
+        private static bool emptySpace;
+        private static List<Apple> appleList = new List<Apple>();
+
+        public static List<Apple> AppleList { get => appleList; set => appleList = value; }
+
         public Apple(Vector2 position, string spriteName, ContentManager content) : base(position, spriteName, content)
         {
             
@@ -23,7 +28,88 @@ namespace Snake
 
         public override void Draw(SpriteBatch spriteBatch)
         {
+            base.Draw(spriteBatch);
+        }
 
+        public static void SpawnApple()
+        {
+            emptySpace = false;
+
+            while (emptySpace == false)
+            {
+                emptySpace = true;
+
+                int xCoordinate = GameWorld.Rng.Next(1, 31);
+                int yCoordinate = GameWorld.Rng.Next(1, 19);
+
+                if (GameWorld.Player == 1)
+                {
+                    foreach (Wall item in GameWorld.wallList)
+                    {
+                        if (item.position == new Vector2(xCoordinate * 30, yCoordinate * 30))
+                        {
+                            emptySpace = false;
+                        }
+                    }
+                }
+
+                if (GameWorld.Player == 2)
+                {
+                    foreach (Wall item in GameWorld.wallList)
+                    {
+                        if (item.position == new Vector2(xCoordinate * 30 + Wall.xJumpLength, yCoordinate * 30))
+                        {
+                            emptySpace = false;
+                        }
+                    }
+                }
+
+                if (GameWorld.Player == 3)
+                {
+                    foreach (Wall item in GameWorld.wallList)
+                    {
+                        if (item.position == new Vector2(xCoordinate * 30, yCoordinate * 30 + Wall.yJumpLength))
+                        {
+                            emptySpace = false;
+                        }
+                    }
+                }
+
+                if (GameWorld.Player == 4)
+                {
+                    foreach (Wall item in GameWorld.wallList)
+                    {
+                        if (item.position == new Vector2(xCoordinate * 30 + Wall.xJumpLength, yCoordinate * 30 + Wall.yJumpLength))
+                        {
+                            emptySpace = false;
+                        }
+                    }
+                }
+
+                if (emptySpace == true)
+                {
+                    if (GameWorld.Player == 1)
+                    {
+                        AppleList.Add(new Apple(new Vector2(xCoordinate * 30, yCoordinate * 30), "Apple", GameWorld.ContentManager));
+                    }
+
+                    if (GameWorld.Player == 2)
+                    {
+                        AppleList.Add(new Apple(new Vector2(xCoordinate * 30 + Wall.xJumpLength, yCoordinate * 30), "Apple", GameWorld.ContentManager));
+                    }
+
+                    if (GameWorld.Player == 3)
+                    {
+                        AppleList.Add(new Apple(new Vector2(xCoordinate * 30, yCoordinate * 30 + Wall.yJumpLength), "Apple", GameWorld.ContentManager));
+                    }
+
+                    if (GameWorld.Player == 4)
+                    {
+                        AppleList.Add(new Apple(new Vector2(xCoordinate * 30 + Wall.xJumpLength, yCoordinate * 30 + Wall.yJumpLength), "Apple", GameWorld.ContentManager));
+                    }
+
+                }
+            }
         }
     }
 }
--- Snake/GameWorld.cs
+++ Snake/GameWorld.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Content;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -20,6 +21,8 @@ namespace Snake
         private static ContentManager content;
         private float delay;
         private Texture2D collisionTexture;
+        private static int player = 1;
+        private static Random rng = new Random();
 
         public static GameObject[,] TileSet = new GameObject[64, 36];
         public static Snakehead head;
@@ -60,6 +63,9 @@ namespace Snake
                 return content;
             }
         }
+
+        public static int Player { get => player; set => player = value; }
+        public static Random Rng { get => rng; set => rng = value; }
 
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
@@ -174,14 +180,40 @@ namespace Snake
 
             base.Update(gameTime);
 
-           if (Keyboard.GetState().IsKeyDown(Keys.E) && delay > 100)
+            if (Keyboard.GetState().IsKeyDown(Keys.D1) && delay > 100)
+            {
+                player = 1;
+                delay = 0;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D2) && delay > 100)
+            {
+                player = 2;
+                delay = 0;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D3) && delay > 100)
+            {
+                player = 3;
+                delay = 0;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D4) && delay > 100)
+            {
+                player = 4;
+                delay = 0;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.E) && delay > 100)
             {
                 //Wall.SpawnEnemyWalls(1,4,9);
                 //Wall.SpawnEnemyWalls(2,40,4);
                 //Wall.SpawnEnemyWalls(3,9,2);
                 //Wall.SpawnEnemyWalls(4,2,14);
 
-                new Snakebody((Snake.snakeParts[Snake.snakeParts.Count-1].position- new Vector2(30,0)),"SnakeBody1",content);
+                //new Snakebody((Snake.snakeParts[Snake.snakeParts.Count-1].position- new Vector2(30,0)),"SnakeBody1",content);
+
+                Apple.SpawnApple();
                 delay = 0;
             }
 
@@ -218,6 +250,11 @@ namespace Snake
             if (test != null)
             {
                 spriteBatch.DrawString(font, test, new Vector2(0), Color.Red);
+            }
+
+            foreach (Apple item in Apple.AppleList)
+            {
+                item.Draw(spriteBatch);
             }
 
             spriteBatch.End();
